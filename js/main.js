99e7c94f836ffbb4ad8999f8afdda5395edd47f9
Naveen <notnaveen2@gmail.com>
2024-01-28 21:06:02 +0530

    added project 2 QR

--- js/main.js
+++ js/main.js
@@ -23,3 +23,18 @@ function scrollToSection(section) {
    // Add your desired functionality here
  }
 
+
+ function QRslideshow() {
+   var QrcImg = document.getElementsByClassName('project-image2')[0]
+   var image = 0;
+   var urls = ['url("../Assets/projects-thumbnail/QRC/QRCGen-DarkMode.png")','url("../Assets/projects-thumbnail/QRC/QRCGen-LightMode.png")',
+   'url("../Assets/projects-thumbnail/QRC/extension-whole.png")',
+]
+   setInterval(() => {
+      QrcImg.style.backgroundImage = urls[image];
+      image = (image + 1) % urls.length; // Increment image index, loop back to 0 if at the end
+   }, 2000);
+ }
+
+QRslideshow();
+
